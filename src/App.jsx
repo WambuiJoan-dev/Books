424d237d11f3f920fce454d62f334bfc579394ac
+ <+>
2025-02-03 02:40:40 +0300

    initialising states

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
   import React from 'react'
+import { useState } from 'react';
   
   function App = () => {
 
@@ -8,6 +9,19 @@
     { id: 2, title: "1984", author: "George Orwell", genre: "Dystopian" },
     { id: 3, title: "To Kill a Mockingbird", author: "Harper Lee", genre: "Classic" },
   ]);
+
+  //adding a new book
+  const addBook = () => {
+    if (newBook.title && newBook.author && newBook.genre){
+        setBooks([...books, {id: books.length + 1, ...newBook}]);
+        setNewBook({title: "", author: "", genre: ""});
+    }
+  }
+
+  //initialising states
+  const [search, setSearch] = useState("");
+  const [newBook, setNewBook] = useState({ title: "", author: "", genre: ""});
+  const [sortBy, setSortBy] = useState("title"); //sort by title
 
     return (
       <div>App</div>
